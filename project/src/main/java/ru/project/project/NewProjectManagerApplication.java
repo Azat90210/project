b73f54dd0c9df3fc1ab52c641e3a18624e352ca8
Mukhs <azat_market@mail.ru>
2023-03-24 20:06:34 +0300

    Initial commit

---
+++ project/src/main/java/ru/project/project/NewProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.project.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewProjectManagerApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/ru/project/project/NewProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
